e79f489ba763456c9a12819563f39b37f6e4b9fb
Christian Mayer <christian@fox21.at>
2014-11-01 20:11:58 +0100

    Test data, int.

--- tests/BinTest.php
+++ tests/BinTest.php
@@ -4,10 +4,14 @@ use TheFox\Utilities\Bin;
 
 class BinTest extends PHPUnit_Framework_TestCase{
 	
-	public function testBin(){
+	public function testData(){
 		$this->assertTrue(true);
 		
 		Bin::debugData('hello world');
+	}
+	
+	public function testInt(){
+		$this->assertTrue(true);
 		
 		Bin::debugInt(123);
 		Bin::debugInt(255);
