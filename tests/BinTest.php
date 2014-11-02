279c0267c8c95be00ab0410e8b92469fbc215098
Christian Mayer <christian@fox21.at>
2014-11-02 11:35:55 +0100

    Test data.

--- tests/BinTest.php
+++ tests/BinTest.php
@@ -10,11 +10,20 @@ class BinTest extends PHPUnit_Framework_TestCase{
 		Bin::debugData('hello world');
 	}
 	
-	public function testInt(){
+	public function providerInt(){
+		$rv = array();
+		$rv[] = array(123);
+		$rv[] = array(255);
+		return $rv;
+	}
+	
+	/**
+	 * @dataProvider providerInt
+	 */
+	public function testInt($n){
 		$this->assertTrue(true);
 		
-		Bin::debugInt(123);
-		Bin::debugInt(255);
+		Bin::debugInt($n);
 	}
 	
 }
