01179a354b83e78daa3b5396a7a9d1238b4edd77
Christian Mayer <christian@fox21.at>
2015-02-19 16:51:31 +0100

    LEB128 un-/signed implementation.

--- src/TheFox/Utilities/Leb128.php
+++ src/TheFox/Utilities/Leb128.php
@@ -1,38 +1,115 @@
 <?php
 
+// https://en.wikipedia.org/wiki/LEB128
 // http://stackoverflow.com/questions/18195415/c-base128-function
 
 namespace TheFox\Utilities;
 
+use RuntimeException;
+use InvalidArgumentException;
+
 class Leb128{
 	
-	public static function encode($x){
-		$buf = '';
+	// Unsigned Encode
+	public static function uencode($x){
+		if($x < 0){
+			throw new InvalidArgumentException("Value can't be < 0. Use sencode().", 10);
+		}
+		
+		$str = '';
 		do{
-			$c = $x & 0x7f;
-			if($x >>= 7){
-				$c |= 0x80;
+			$char = $x & 0x7f;
+			$x >>= 7;
+			if($x > 0){
+				$char |= 0x80;
 			}
-			$buf .= chr($c);
+			$str .= chr($char);
 		}while($x);
-		return $buf;
+		return $str;
 	}
 	
-	public static function decode($str, &$x){
+	// Unsigned Decode
+	public static function udecode($str, &$x, $maxlen = 16){
 		$len = 0;
 		$x = 0;
 		while($str){
-			$c = substr($str, 0, 1);
-			$c = ord($c);
+			$char = substr($str, 0, 1);
+			$char = ord($char);
 			$str = substr($str, 1);
 			
-			$x |= ($c & 0x7f) << (7 * $len);
+			$x |= ($char & 0x7f) << (7 * $len);
 			$len++;
 			
-			if(!($c & 0x80)){
+			#Bin::debugInt($char);
+			
+			if(($char & 0x80) == 0){
 				break;
 			}
+			
+			if($len >= $maxlen){
+				throw new RuntimeException('Max length '.$maxlen.' reached.', 20);
+			}
 		}
+		return $len;
+	}
+	
+	// Signed Encode
+	public static function sencode($x){
+		$buf = '';
+		$more = 1;
+		$negative = $x < 0;
+		while($more){
+			$char = $x & 0x7f;
+			$x >>= 7;
+			
+			/*if($negative){
+				$intSize = 64;
+				$x |= -(1 << ($intSize - 7));
+			}*/
+			
+			if(($x == 0 && ($char & 0x40) == 0) || ($x == -1 && ($char & 0x40))){
+				$more = 0;
+			}
+			else{
+				$char |= 0x80;
+			}
+			$buf .= chr($char);
+		}
+		
+		return $buf;
+	}
+	
+	// Signed Decode
+	public static function sdecode($str, &$x, $maxlen = 16, $intSize = 64){
+		$len = 0;
+		$x = 0;
+		$char = 0;
+		$shift = 0;
+		while($str){
+			$char = substr($str, 0, 1);
+			$char = ord($char);
+			$str = substr($str, 1);
+			
+			$shift = 7 * $len;
+			$x |= ($char & 0x7f) << $shift;
+			$len++;
+			
+			if(($char & 0x80) == 0){
+				break;
+			}
+			
+			if($len >= $maxlen){
+				throw new RuntimeException('Max length '.$maxlen.' reached.', 30);
+			}
+		}
+		
+		$shift = 7 * $len;
+		
+		// Sign bit of byte is 2nd high order bit (0x40)
+		if($shift < $intSize && ($char & 0x40)){
+			$x |= -(1 << $shift);
+		}
+		
 		return $len;
 	}
 	
--- tests/Leb128Test.php
+++ tests/Leb128Test.php
@@ -4,32 +4,78 @@ use TheFox\Utilities\Leb128;
 
 class Leb128Test extends PHPUnit_Framework_TestCase{
 	
-	public function testEncode(){
-		$actual = unpack('H*', Leb128::encode(624485));
+	public function testUencode(){
+		$actual = unpack('H*', Leb128::uencode(0));
+		$actual = $actual[1];
+		$this->assertEquals('00', $actual);
+		
+		$actual = unpack('H*', Leb128::uencode(624485));
 		$actual = $actual[1];
 		$this->assertEquals('e58e26', $actual);
 		
-		$actual = unpack('H*', Leb128::encode(1024 * 1024 * 1024));
+		$actual = unpack('H*', Leb128::uencode(1024 * 1024 * 1024));
 		$actual = $actual[1];
 		$this->assertEquals('8080808004', $actual);
 	}
 	
-	public function testDecode(){
-		$len = Leb128::decode(pack('H*', 'e58e26'), $x);
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionCode 10
+	 */
+	public function testUencodeException10(){
+		$actual = unpack('H*', Leb128::uencode(-624485));
+	}
+	
+	/**
+	 * @expectedException RuntimeException
+	 * @expectedExceptionCode 20
+	 */
+	public function testUencodeException20(){
+		$len = Leb128::udecode(pack('H*', '8080808004FF'), $x, 4);
+		$this->assertEquals(1024 * 1024 * 1024, $x);
+		$this->assertEquals(5, $len);
+	}
+	
+	public function testUdecode(){
+		$len = Leb128::udecode(pack('H*', 'e58e26'), $x);
 		$this->assertEquals(624485, $x);
 		$this->assertEquals(3, $len);
 		
-		$len = Leb128::decode(pack('H*', 'e58e26FF'), $x);
+		$len = Leb128::udecode(pack('H*', '8080808004'), $x);
+		$this->assertEquals(1024 * 1024 * 1024, $x);
+		$this->assertEquals(5, $len);
+		
+		$len = Leb128::udecode(pack('H*', '8080808004'), $x, 5);
+		$this->assertEquals(1024 * 1024 * 1024, $x);
+		$this->assertEquals(5, $len);
+		
+		$len = Leb128::udecode(pack('H*', '8080808004FFFF'), $x);
+		$this->assertEquals(1024 * 1024 * 1024, $x);
+		$this->assertEquals(5, $len);
+		
+		$len = Leb128::udecode(pack('H*', '8080808004FFFF'), $x, 5);
+		$this->assertEquals(1024 * 1024 * 1024, $x);
+		$this->assertEquals(5, $len);
+	}
+	
+	public function testSencode(){
+		$actual = unpack('H*', Leb128::sencode(-624485));
+		$actual = $actual[1];
+		$this->assertEquals('9bf159', $actual);
+		
+		$actual = unpack('H*', Leb128::sencode(624485));
+		$actual = $actual[1];
+		$this->assertEquals('e58e26', $actual);
+	}
+	
+	public function testSdecode(){
+		$len = Leb128::sdecode(pack('H*', 'e58e26'), $x);
 		$this->assertEquals(624485, $x);
 		$this->assertEquals(3, $len);
 		
-		$len = Leb128::decode(pack('H*', '8080808004'), $x);
-		$this->assertEquals(1024 * 1024 * 1024, $x);
-		$this->assertEquals(5, $len);
-		
-		$len = Leb128::decode(pack('H*', '8080808004FFFFFF'), $x);
-		$this->assertEquals(1024 * 1024 * 1024, $x);
-		$this->assertEquals(5, $len);
+		$len = Leb128::sdecode(pack('H*', '9bf159'), $x);
+		$this->assertEquals(-624485, $x);
+		$this->assertEquals(3, $len);
 	}
 	
 }
