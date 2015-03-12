5bf3c1a4ca25c371183358777eb2c02c36f4b99b
Christian Mayer <christian@fox21.at>
2015-03-12 23:05:55 +0100

    Version 1.8.0.

--- src/TheFox/Utilities/Utilities.php
+++ src/TheFox/Utilities/Utilities.php
@@ -5,7 +5,7 @@ namespace TheFox\Utilities;
 class Utilities{
 	
 	const NAME = 'Utilities';
-	const VERSION = '1.8.0-dev';
-	const RELEASE = 13;
+	const VERSION = '1.8.0';
+	const RELEASE = 14;
 	
 }
